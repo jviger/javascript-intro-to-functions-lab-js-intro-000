bbba91de33ec172ade293f40aa89d1971f0a9c6d
Jason Viger <jason.viger@gmail.com>
2018-07-07 11:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,5 @@ function sayHiToGrandma(string){
    if (string === string.toUpperCase()){
      return "YES INDEED!";}
      else if (string === "I love you, Grandma."){ return "I love you, too."}
-   }
- } 
-}
+   
 }
